410d17329e1c64ef08739dbb9a70b6a79e5ac290
Pierre Cochard <pchd@quarre.fr>
2019-08-13 17:14:52 +0200

    bundle sc file

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 yes | cp -rf overwrites/lang/CMakeLists.txt dependencies/supercollider/lang
 yes | cp -rf overwrites/lang/LangPrimSource/OSCData.cpp dependencies/supercollider/lang/LangPrimSource
 yes | cp -rf overwrites/lang/LangPrimSource/PyrPrimitive.cpp dependencies/supercollider/lang/LangPrimSource
+yes | cp -rf sclang/WebSocket dependencies/supercollider/SCClassLibrary/Common
+
 
 mkdir build
 cd build 
